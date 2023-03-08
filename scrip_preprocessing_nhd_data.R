d75e4ba3df3545dad5dbb79d4659a88d80f2ba7c
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2023-03-07 19:44:50 -0800

    Added shapefiles (temporarily)

--- scrip_preprocessing_nhd_data.R
+++ scrip_preprocessing_nhd_data.R
@@ -20,12 +20,24 @@ set.seed(2703)
 # Import / Export of assets
 
 # Import: Repository path to raw data
-raw_data <- "https://raw.githubusercontent.com/Scaling-Watershed-Function/1-swf-knowledge.base/main/assets/data/raw/"
+raw_data <- "https://raw.githubusercontent.com/Scaling-Watershed-Function/1-swf-knowledge.base/main/assets/data/raw/shapes/"
 
 # Loading NHD data
 
 # Local import
-assets_data <- "..\1-swf-knowledge.base\assets\data\raw\shapes"
+# assets_data <- "../2-swf-analytical.engine/shapes" It does not work
+
 
 require(sf)
-nhd_yrb_stream <- st_read(assets_data,"nhd_CR_stream_sub8.shp")
+# nhd_yrb_stream <- st_read(assets_data,"nhd_CR_stream_sub8.shp") it does not work
+
+# Apparently files have to be rigth outside in the working directory
+
+nhd_yrb_stream <- st_read("nhd_CR_stream_sub8.shp")
+
+tmp<-st_zm(nhd_yrb_stream)
+nhd_yrb_poly<-tmp[,"COMID"]
+nhd_yrb_stream<-data.frame(nhd_yrb_poly)
+nhd_yrb_stream$COMID<-NULL                          
+
+
