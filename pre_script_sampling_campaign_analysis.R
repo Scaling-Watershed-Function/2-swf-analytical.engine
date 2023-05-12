ffd17081515fe2dca4d500053efe5296b8d42bbb
Francisco J. Guerrero <guerrero.francisco.jose@gmail.com>
2023-05-12 05:34:41 -0700

    Update pre_script_sampling_campaign_analysis.R

--- pre_script_sampling_campaign_analysis.R
+++ pre_script_sampling_campaign_analysis.R
@@ -66,27 +66,106 @@ set.seed(2703)
 
 # External data import
 
-heading_dat_path <- 
+heading_dat_path <- "https://media.githubusercontent.com/media/Scaling-Watershed-Function/1-swf-knowledge.base/main/assets/data/processed/230429_dd_basin_char_hydr_geom_yrb_wrb.csv"
+bgc_dat_path <- "https://media.githubusercontent.com/media/Scaling-Watershed-Function/1-swf-knowledge.base/main/assets/data/processed/230507_bgc_dat_c10.csv"
+spat_study_path <- "https://media.githubusercontent.com/media/Scaling-Watershed-Function/1-swf-knowledge.base/main/assets/data/raw/combined_results_updated_04262023.csv"
 
-heading_dat <- read_csv(url = "https://media.githubusercontent.com/media/Scaling-Watershed-Function/1-swf-knowledge.base/main/assets/data/raw/230429_dd_basin_char_hydr_geom_yrb_wrb.csv",
-                        show_col)
+heading_dat <- read_csv(heading_dat_path,show_col_types = FALSE)
+bgc_dat_c10 <- read_csv(bgc_dat_path, show_col_types = FALSE)
+spat_study <- read_csv(spat_study_path, show_col_types = FALSE)
 
 
+# Local import and export paths (when clonning the entire project)
 
-# Local import and export paths
+# raw_data <- "../1-swf-knowledge.base/assets/data/raw" 
+# processed_data <- "../1-swf-knowledge.base/assets/data/processed"
+# assets_figs <- "../1-swf-knowledge.base/assets/plots"
+# 
+# #header info (data dictionary)
+# 
+# heading_dat <- read_csv(paste(processed_data,"guerrero_etal_swf_dd.csv", sep = '/'),
+#                         show_col_types = FALSE)
+# 
+# #values
+# bgc_dat_c10 <- read_csv(paste(processed_data,"230507_bgc_dat_c10.csv", sep = "/"),
+#                         show_col_types = FALSE)
+# 
+# spat_stdy <- read_csv(paste(raw_data,"combined_results_updated_04262023.csv", sep = "/"),
+#                       show_col_types = FALSE)
 
-raw_data <- "../1-swf-knowledge.base/assets/data/raw" 
-processed_data <- "../1-swf-knowledge.base/assets/data/processed"
-assets_figs <- "../1-swf-knowledge.base/assets/plots"
+# Merging bgc data with the spatial study dataset
 
-#header info (data dictionary)
+ykm_spc <- spat_study %>% 
+  mutate(tot_o2_consump_gm2day = -ERsed_gm2day) %>% 
+  rename(comid = COMID.y) %>% 
+  merge(.,
+        bgc_dat_c10,
+        by = "comid",
+        all.x = TRUE) %>% 
+  select(-COMID.x) %>% 
+  filter(ERsed_gm2day < 0)
 
-heading_dat <- read_csv(paste(processed_data,"guerrero_etal_swf_dd.csv", sep = '/'),
-                        show_col_types = FALSE)
+# Quick exploratory plots
 
-#values
-bgc_dat_c10 <- read_csv(paste(processed_data,"230507_bgc_dat_c10.csv", sep = "/"),
-                        show_col_types = FALSE)
+# checking relationship between o2 consumption and streambed area
 
-spat_stdy <- read_csv(paste(raw_data,"combined_results_updated_04262023.csv", sep = "/"),
-                      show_col_types = FALSE)
+o2_area_mod <- lm(tot_o2_consump_gm2day~stream_area_m2,
+                   data = ykm_spc)
+summary(o2_area_mod)
+
+# R squared is 0.27 and p value < 0.0001
+
+plot(o2_area_mod) #regression diagnostic plots
+# Observations 3, 6, and 37 seem to be highly influential on the regression plot
+
+inf_obs <- ykm_spc %>% 
+  slice(1,3,6,37)
+inf_obs
+
+# Running model without influential observations
+o2_area_mod_ni <- lm(tot_o2_consump_gm2day~wshd_area_km2,
+                     data = ykm_spc[-1,])
+summary(o2_area_mod_ni)
+plot(o2_area_mod_ni)
+
+# R squared drops to 0.05 and p value is 0.12
+
+# Thus the trend in the plot for watershed area should exclude these influential observations: 
+
+# Let's start with the multiple regression plot
+
+o2_mr_mod <- lm(tot_o2_consump_gm2day ~ log(mean_depth_m) + log(velocity_ms) + log(wshd_area_km2) + slope,
+               data = ykm_spc[-1,])
+
+
+summary(o2_mr_mod)
+crPlots(o2_mr_mod)
+vif(o2_mr_mod)
+
+o2_mr_mod2 <- lm(tot_o2_consump_gm2day ~ log(mean_depth_m) + log(velocity_ms) + log(wshd_area_km2) + slope,
+                data = ykm_spc)
+
+
+summary(o2_mr_mod2)
+crPlots(o2_mr_mod2)
+vif(o2_mr_mod2)
+
+# Bringing back in the influential variables into a multiple regression analysis does not change the results
+
+# Let's plot respiration rates vs. watershed area, but excluding the influential observation from the trend
+
+# Running model without influential observations (watershed area)
+o2_warea_mod_ni <- lm(tot_o2_consump_gm2day~wshd_area_km2,
+                     data = ykm_spc[-1,])
+summary(o2_warea_mod_ni)
+plot(o2_warea_mod_ni)
+
+o2_stream_area_ni <- ggplot(data =  ykm_spc[-c(1,3,6),],
+                            aes(x = wshd_area_km2,
+                                y = tot_o2_consump_gm2day))+
+  geom_smooth(method = "lm")+
+  geom_point()+
+  scale_x_log10()+
+  scale_y_log10()
+o2_stream_area_ni
+  
