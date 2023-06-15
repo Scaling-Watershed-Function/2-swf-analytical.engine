936d55baba3493f4f52b197c1b1e17d81e4f0065
Francisco J. Guerrero <guerrero.francisco.jose@gmail.com>
2023-06-15 12:36:59 -0700

    Update pre_script_hydrobiogeochemistry_son_etal_22.R

--- pre_script_hydrobiogeochemistry_son_etal_22.R
+++ pre_script_hydrobiogeochemistry_son_etal_22.R
@@ -17,11 +17,45 @@ processed_data <- "../1-swf-knowledge.base/assets/data/processed"
 phys_dat_mod8 <- read_csv(paste(raw_data,"230504_phys_dat_mod8.csv", sep = "/"),
                           show_col_types = FALSE)
 
+# Artificial paths
+art_path <- nrow(filter(phys_dat_mod8, reach_type == "ArtificialPath"))
+# Canals and Ditches
+can_dtch <- nrow(filter(phys_dat_mod8, reach_type == "CanalDitch"))
+# Connectors
+con_path <- nrow(filter(phys_dat_mod8, reach_type == "Connector"))
+
+# Natural Channels
+phys_dat_mod9 <- filter(phys_dat_mod8, reach_type == "StreamRiver")
+
+
+summary(filter(phys_dat_mod9, basin == "yakima"))
+summary(filter(phys_dat_mod9, basin == "willamette"))
+
+# Total flow lines corresponding to natural channels in Yakima is 5978 and 8634 for Willamette
+
 bgc_dat0 <- read_csv(paste(raw_data,"230406_son_etal_22_results_zen.csv", sep = "/"),
                     show_col_types = FALSE)
 
+# Extracting Son et al's., data for Willamete and Yakima
 
-bgc_dat <- phys_dat_mod8 %>% 
+son_pnw_dat_wo_na <- filter(bgc_dat0, is.na(pred_do)==FALSE)
+
+son_pnw_dat <- filter(son_pnw_dat_wo_na, time_type == "annual")
+summary(son_pnw_dat)
+
+son_pnw_nat <- son_pnw_dat %>% 
+  merge(.,
+        phys_dat_mod9,
+        by = "comid",
+        all.x = TRUE) %>% 
+  filter(reach_type == "StreamRiver")
+
+# It yields 12755 observations with no NAs. So the current dataset would be reduced 1857 observations, 
+# equivalent to 13 %
+summary(son_pnw_nat)
+
+
+bgc_dat <- phys_dat_mod9 %>% 
   merge(.,
         bgc_dat0 %>%
           filter(time_type=="annual") %>% 
@@ -56,6 +90,22 @@ rt_mod <- lm(logRT_total_hz_s ~ log(reach_slope) +
              na.action = na.omit)
 summary(rt_mod)
 
+bgc_dat_c1_valid <- bgc_dat %>% 
+  select(logRT_total_hz_s,
+         reach_slope,
+         basin,
+         stream_order,
+         mean_ann_flow_m3s,
+         wshd_area_km2,
+         reach_length_km) %>% 
+  mutate(pred_lgRT = predict.lm(rt_mod,.)) %>% 
+  ggplot(aes(logRT_total_hz_s,
+             pred_lgRT,
+             color = basin))+
+  geom_point()+
+  geom_abline()+
+  facet_wrap(~basin)
+bgc_dat_c1_valid
 
 bgc_dat_c1 <- bgc_dat %>% 
   mutate(logRT_total_hz_s = if_else(is.na(logRT_total_hz_s),
@@ -69,22 +119,48 @@ bgc_plot2 <- na.omit(bgc_dat) %>%
   ggplot(aes(x = logRT_total_hz_s,
              y = logq_hz_total_m_s,
              color = as.factor(stream_order)))+
-  scale_x_log10()+
+  # scale_x_log10()+
   geom_point()+
   facet_wrap(basin~as.factor(stream_order),ncol = 7)
 bgc_plot2
 
 # hz mod
 
-hz_mod <- lm(logq_hz_total_m_s ~ log(reach_slope) + 
-               basin + 
-               stream_order+
-               log(mean_ann_flow_m3s)+
-               log(wshd_area_km2)+
-               log(reach_length_km),
-             data = bgc_dat,
+hz_mod <- bgc_dat_c1 %>% 
+  select(logRT_total_hz_s,
+         logq_hz_total_m_s,
+         reach_slope,
+         basin,
+         stream_order,
+         mean_ann_flow_m3s,
+         wshd_area_km2,
+         reach_length_km) %>% 
+  mutate(logRT2 = logRT_total_hz_s^2) %>% 
+  lm(logq_hz_total_m_s ~ logRT_total_hz_s+
+              logRT2,
+             data = .,
              na.action = na.omit)
 summary(hz_mod)
+
+bgc_dat_c2_valid <- bgc_dat %>% 
+  select(logRT_total_hz_s,
+         logq_hz_total_m_s,
+         reach_slope,
+         basin,
+         stream_order,
+         mean_ann_flow_m3s,
+         wshd_area_km2,
+         reach_length_km) %>% 
+  mutate(logRT2 = logRT_total_hz_s^2) %>% 
+  mutate(pred_logq_hz = predict.lm(hz_mod,.)) %>% 
+  ggplot(aes(logq_hz_total_m_s,
+             pred_logq_hz,
+             color = basin))+
+  geom_point()+
+  geom_abline()+
+  facet_wrap(~basin)
+bgc_dat_c2_valid
+
 
 bgc_dat_c2 <- bgc_dat_c1 %>% 
   mutate(logq_hz_total_m_s = if_else(is.na(logq_hz_total_m_s),
