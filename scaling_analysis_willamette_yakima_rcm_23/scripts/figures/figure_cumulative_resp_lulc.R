b1b71bb250e1c8fbea2d1e58314ec13d15b5c11e
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2023-10-09 07:49:05 -0700

    Update figure_cumulative_resp_lulc.R

--- scaling_analysis_willamette_yakima_rcm_23/scripts/figures/figure_cumulative_resp_lulc.R
+++ scaling_analysis_willamette_yakima_rcm_23/scripts/figures/figure_cumulative_resp_lulc.R
@@ -22,39 +22,206 @@ source("./source/script_graphic_prep_design.R")
 
 landuse_scaling_dat <- scaling_analysis_dat %>% 
   select(wshd_area_km2,
+         basin,
          basin_cat,
          sto_fct,
          accm_hzt_cat,
-         w_ht,
-         frst_cat,
-         accm_water_exchng_kg_day,
+         ht_3,
+         hrel_3,
+         hr3_cat,
+         accm_ent_cat,
+         frs3_cat,
+         accm_water_exchng_kg_d,
          accm_totco2_o2g_day,
          accm_totco2_ang_day,
-         forest_scp_3,
-         humans_scp_3,
-         shrubl_scp_3,
-         mean_ann_runf_mm) %>% 
-  gather(c(10:12),key="use",value = "fraction") %>% 
-  mutate(use = fct_relevel(use,c("forest_scp_3","shrubl_scp_3","humans_scp_3"))) %>% 
+         forest_3scp,
+         human_3scp,
+         shrub_3scp,
+         accm_mean_ann_runf_mm) %>% 
+  gather(c(forest_3scp,
+           human_3scp,
+           shrub_3scp),
+         key="use",
+         value = "fraction") %>% 
+  mutate(use = fct_relevel(use,c("forest_3scp","shrub_3scp","human_3scp"))) %>% 
   arrange(use) 
 
-# Test plot
+# Willamette River Basin
 
-p <- ggplot(data = landuse_scaling_dat,
-            aes(x = wshd_area_km2,
-                y = accm_totco2_o2g_day/wshd_area_km2,
-                color =log(mean_ann_runf_mm)))+
-  facet_wrap(basin_cat~frst_cat,nrow = 2)+
-  scale_x_log10()+
-  scale_y_log10()+
-  geom_point()
-p
+accm_w_resp_rates_lulc <- ggplot(data = filter(landuse_scaling_dat, basin == "willamette"),
+                                 aes(x = wshd_area_km2,
+                                     y = accm_totco2_o2g_day/wshd_area_km2,
+                                     color = use))+
+  geom_point(aes(alpha = fraction), size = 3.5)+
+  scale_x_log10(breaks = breaks, 
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.01,30000)) +
+  scale_y_log10(breaks = breaks_c,
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.001,500000)) +
+  scale_color_manual(name = "Land cover",
+                     values = c("#008837", "#FFC618", "#7b3294"),
+                     labels = c("Forestscapes", "Shrublandscapes", "Humanscapes")) +
+  annotation_logticks(size = 0.75, sides = "tblr") +
+  annotate(geom="text",
+           x=0.025,
+           y=340000,
+           label='bold("A")',
+           parse = TRUE,
+           color="black",
+           size = 14)+
+  theme_httn+
+  theme(legend.position = c(0.90,0.15),
+               legend.title = element_text(size = 24, face = "bold"),
+               legend.text = element_text(size = 18),
+               axis.title = element_blank(),
+               axis.text.x = element_blank(),
+               axis.text.y = element_text(size = 32),
+               plot.margin = margin(0, 0, 0, 0, "cm"),
+               plot.title = element_text(size = 32, face ="bold"))+
+  guides(alpha = "none")
+accm_w_resp_rates_lulc 
 
 
-summary(scaling_analysis_dat)
+accm_w_resp_rates_hrel <- ggplot(data = filter(landuse_scaling_dat, basin == "willamette"),
+                                 aes(x = wshd_area_km2,
+                                     y = accm_totco2_o2g_day/wshd_area_km2,
+                                     color = hr3_cat))+
+  geom_point(aes(alpha = fraction), size = 3.5)+
+  scale_x_log10(breaks = breaks, 
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.01,30000)) +
+  scale_y_log10(breaks = breaks_c,
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.001,500000)) +
+  scale_color_manual(name = "Relative \nentropy \nquantiles",
+                     values = my_mcolors) +
+  annotation_logticks(size = 0.75, sides = "tblr") +
+  annotate(geom="text",
+           x=0.025,
+           y=340000,
+           label='bold("B")',
+           parse = TRUE,
+           color="black",
+           size = 14)+
+  theme_httn+
+  theme(legend.position = c(0.90,0.15),
+        legend.title = element_text(size = 24, face = "bold"),
+        legend.text = element_text(size = 18),
+        axis.title = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 32),
+        plot.margin = margin(0, 0, 0, 0, "cm"),
+        plot.title = element_text(size = 32, face ="bold"))+
+  guides(alpha = "none")
+accm_w_resp_rates_hrel 
+
+w_combined_plot <- grid.arrange(accm_w_resp_rates_lulc , 
+                                accm_w_resp_rates_hrel , 
+                                ncol = 2)
+w_combined_plot
+
+# Yakima River Basin
+
+accm_y_resp_rates_lulc <- ggplot(data = filter(landuse_scaling_dat, basin == "yakima"),
+                                 aes(x = wshd_area_km2,
+                                     y = accm_totco2_o2g_day/wshd_area_km2,
+                                     color = use))+
+  geom_point(aes(alpha = fraction), size = 3.5)+
+  scale_x_log10(breaks = breaks, 
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.01,30000)) +
+  scale_y_log10(breaks = breaks_c,
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.001,500000)) +
+  scale_color_manual(name = "Land cover",
+                     values = c("#008837", "#FFC618", "#7b3294"),
+                     labels = c("Forestscapes", "Shrublandscapes", "Humanscapes")) +
+  annotation_logticks(size = 0.75, sides = "tblr") +
+  annotate(geom="text",
+           x=0.025,
+           y=340000,
+           label='bold("C")',
+           parse = TRUE,
+           color="black",
+           size = 14)+
+  theme_httn+
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+        axis.text = element_text(size = 32),
+        plot.margin = margin(-0.1, 0, -0.1, 0, "cm"),
+        plot.title = element_text(size = 32, face ="bold"))
+accm_y_resp_rates_lulc 
+
+
+accm_y_resp_rates_hrel <- ggplot(data = filter(landuse_scaling_dat, basin == "yakima"),
+                                 aes(x = wshd_area_km2,
+                                     y = accm_totco2_o2g_day/wshd_area_km2,
+                                     color = hr3_cat))+
+  geom_point(aes(alpha = fraction), size = 3.5)+
+  scale_x_log10(breaks = breaks, 
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.01,30000)) +
+  scale_y_log10(breaks = breaks_c,
+                labels = trans_format("log10", math_format(10^.x)),
+                limits = c(0.001,500000)) +
+  scale_color_manual(name = "Relative \nentropy \nquantiles",
+                     values = my_mcolors) +
+  annotation_logticks(size = 0.75, sides = "tblr") +
+  annotate(geom="text",
+           x=0.025,
+           y=340000,
+           label='bold("D")',
+           parse = TRUE,
+           color="black",
+           size = 14)+
+  theme_httn+
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+        axis.text = element_text(size = 32),
+        plot.margin = margin(-0.1, 0, -0.1, 0, "cm"),
+        plot.title = element_text(size = 32, face ="bold"))+
+  guides(alpha = "none")
+accm_y_resp_rates_hrel 
+
+
+y_combined_plot <- grid.arrange(accm_y_resp_rates_lulc , 
+                                accm_y_resp_rates_hrel , 
+                                ncol = 2)
+y_combined_plot
+
+
+all_combined_plot <- grid.arrange(w_combined_plot,
+                                  y_combined_plot,
+                                  left = textGrob(expression(bold(paste("Cumulative aerobic"," ", respiration[Hyp],"(", gCO[2] * d^-1 * km^-2, ")"))),
+                                                  rot = 90,
+                                                  just = "centre",
+                                                  gp = gpar(col = "black", fontsize = 44)),
+                                  bottom = textGrob(expression(bold(paste("Watershed area"," ","(", km^2, ")"))),
+                                                    gp = gpar(col = "black", fontsize = 44)),
+                                  nrow = 2)
+all_combined_plot
+
+
+ggsave(file=paste(results_png, paste0("guerrero_etal_23_cumulative_ab_resp_wyrb_lulc.png"),sep = '/'),
+       all_combined_plot,
+       width = 32,
+       height = 24,
+       units = "in")
 
 
 
+
+
+
+
+
+
+
+
+
+
+#################################################################################
 #Plotting function
 generate_lnd_plot <- function(data, accm_var, ylab_expression, wrap_col) {
   ggplot_obj <- ggplot(data = data,
