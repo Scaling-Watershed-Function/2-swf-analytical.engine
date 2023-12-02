83aaed3d5bb1a718dd1cade4e1c9c241e3afec6c
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2023-12-01 22:18:08 -0800

    Update figure_scaling_maps.R

--- scaling_analysis_willamette_yakima_rcm_23/scripts/figures/figure_scaling_maps.R
+++ scaling_analysis_willamette_yakima_rcm_23/scripts/figures/figure_scaling_maps.R
@@ -16,6 +16,23 @@ cat(readLines("./metadata/code_instructions.Rmd"),sep = '\n')
 # Plot settings
 source("./source/design_scaling_graphic_prep.R")
 
+# Scaling analysis results
+
+slopes_ci <- read_csv(
+  paste(results,
+        "guerrero_etal_23_results_cross_validation_block_bootstrap_scaling.csv",
+        sep = '/'),
+  show_col_types = FALSE
+) %>% 
+  select(basin,
+         quantile,
+         SlopeCI_2.5,
+         SlopeCI_97.5,
+         RSquared) %>% 
+  rename(slope_l_ci = SlopeCI_2.5,
+         slope_u_ci = SlopeCI_97.5,
+         r_squared = RSquared) %>% 
+  mutate(b_q = paste(basin,quantile,sep = "_"))
 
 # Scaling Maps
 
@@ -28,38 +45,36 @@ scaling_map_dat <- scaling_analysis_dat %>%
          wshd_avg_elevation_m,
          hrel_3,
          accm_hzt_cat) %>% 
+  mutate(b_q = paste(basin,accm_hzt_cat,sep = "_")) %>% 
+  merge(.,
+        slopes_ci %>% 
+          select(slope_l_ci,
+                 slope_u_ci,
+                 r_squared,
+                 b_q),
+        by = "b_q",
+        all.x = TRUE) %>% 
+  select(-b_q) %>% 
   mutate(
-    quantile_num = as.numeric(sub("[^0-9]+", "", accm_hzt_cat)),
-    scaling_cat = 
-           case_when(basin == "willamette" & quantile_num >= 60 ~ "Superlinear",
-                     basin == "willamette" & quantile_num >= 40 & quantile_num < 60 ~ "Linear",
-                     basin == "willamette" & quantile_num < 40 ~ "Uncertain",
-                     basin == "yakima" & quantile_num >= 80 ~ "Superlinear",
-                     basin == "yakima" & quantile_num == 70 ~ "Linear",
-                     basin == "yakima" & quantile_num >= 40 & quantile_num < 70 ~ "Sub-linear",
-                     basin == "yakima" & quantile_num < 40 ~"Uncertain"),
-    scaling_cat = factor(scaling_cat, levels = c("Uncertain",
-                                                 "Sub-linear",
-                                                 "Linear",
-                                                 "Superlinear")),
-    log_mean_ann_pcpt_mm = log(mean_ann_pcpt_mm),
-    scaling_cat_num = 
-      case_when(scaling_cat == "Uncertain"~1,
-                scaling_cat == "Sub-linear"~2,
-                scaling_cat == "Linear"~3,
-                scaling_cat == "Superlinear"~4)
+    scaling_cat = case_when(slope_l_ci <= 1 & slope_u_ci >= 1 & r_squared > 0.8 ~ "Linear",
+                            slope_l_ci < 1 & slope_u_ci < 1 & r_squared > 0.8 ~ "Sublinear",
+                            slope_l_ci > 1 & slope_u_ci > 1 & r_squared > 0.8 ~ "Superlinear",
+                            r_squared < 0.8 ~ "Uncertain"),
+    scaling_cat = factor(scaling_cat, levels = c("Uncertain","Sublinear","Linear","Superlinear")),
+    log_mean_ann_pcpt_mm = log(mean_ann_pcpt_mm)
   )
 
 ###############################################################################
 
 generate_plot <- function(data, aes_color_var, scale_color_func, color_name, title, add_y_axis_label = FALSE, ...) {
   plot <- ggplot(data, aes(x = longitude, y = latitude)) +
-    geom_point(aes(color = .data[[aes_color_var]])) +
+    geom_point(aes(color = .data[[aes_color_var]]),
+               size = 2.125) +
     scale_color_func(name = color_name, ...) +
     facet_wrap(~basin_cat, ncol = 1, scales = "free") +
     labs(title = title) +
     theme(
-      legend.position = c(0.8, 0.3),
+      legend.position = c(0.85, 0.405),
       legend.title = element_text(size = 16, face = "bold"),
       legend.text = element_text(size = 12),
       axis.title = element_blank(),
@@ -78,10 +93,10 @@ generate_plot <- function(data, aes_color_var, scale_color_func, color_name, tit
 }
 
 # Generate each plot using the function
-scaling_plot <- generate_plot(scaling_map_dat, "scaling_cat", scale_color_viridis_d, "Scaling type", "Scaling", TRUE)
-precipt_plot <- generate_plot(scaling_map_dat, "log_mean_ann_pcpt_mm", scale_color_viridis_c, "Precipitation \nlog[mm]", "Precipitation")
-elevat_plot <- generate_plot(scaling_map_dat, "wshd_avg_elevation_m", scale_color_gradientn, "Elevation (m)", "Elevation", colours = terrain.colors(10))
-heterog_plot <- generate_plot(scaling_map_dat, "hrel_3", scale_color_viridis_c, "Entropy \n(relative)", "Landscape entropy", option = "mako")
+scaling_plot <- generate_plot(scaling_map_dat, "scaling_cat", scale_color_viridis_d, "Type", "Scaling", TRUE)
+precipt_plot <- generate_plot(scaling_map_dat, "log_mean_ann_pcpt_mm", scale_color_viridis_c, "Log[mm]", "Precipitation")
+elevat_plot <- generate_plot(scaling_map_dat, "wshd_avg_elevation_m", scale_color_gradientn, "Meters", "Elevation", colours = terrain.colors(10))
+heterog_plot <- generate_plot(scaling_map_dat, "hrel_3", scale_color_viridis_c, "Shannon's h", "Landscape entropy", option = "mako")
 
 
 # Combine the plots side by side
